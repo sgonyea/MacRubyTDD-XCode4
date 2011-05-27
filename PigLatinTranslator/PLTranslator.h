a14bd70e1a68b44dd2c2fc28ae6fc4c698b75487
Scott Gonyea <me@sgonyea.com>
2011-05-26 22:30:03 -0700

    Initial commit

---
+++ PigLatinTranslator/PLTranslator.h
@@ -0,0 +1,17 @@
+//
+//  PLTranslator.h
+//  PigLatinTranslator
+//
+//  Created by Scott Gonyea on 5/26/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface PLTranslator : NSObject {
+@private
+    
+}
+
+@end
---
+++ PigLatinTranslator/PLTranslator.m
@@ -0,0 +1,29 @@
+//
+//  PLTranslator.m
+//  PigLatinTranslator
+//
+//  Created by Scott Gonyea on 5/26/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "PLTranslator.h"
+
+
+@implementation PLTranslator
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+@end
